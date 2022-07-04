f6c7d81f0fe5e8e14f49ac5659dbbeaa7050e6b2
Rohit Tokala <rtokala@purdue.edu>
2022-07-05 00:23:54 +0400

    Add header styles, tailwindcss, mui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,24 @@
-const Header = () => {};
+import { Badge } from "@material-ui/core";
+import { ShoppingCartRounded } from "@material-ui/icons";
+
+const Header = () => {
+  return (
+    <div className=" flex justify-between p-6 items-center">
+      <div className=" pl-10">
+        <h1 className=" text-4xl font-bold">Geekables</h1>
+      </div>
+      <ul className=" flex gap-5 pr-5 justify-evenly">
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+        <li>
+          <Badge>
+            <ShoppingCartRounded />
+          </Badge>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Header;
