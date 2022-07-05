2790fa439b8b1665be354a42090b6c8c83493df6
Rohit Tokala <rtokala@purdue.edu>
2022-07-06 00:18:46 +0400

    Add style components of product cards

--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -1,5 +1,6 @@
 import Products from "./products/Products";
 import uniqid from "uniqid";
+import { ShoppingCartOutlined } from "@material-ui/icons";
 
 const Shop = ({
   universe,
@@ -12,16 +13,38 @@ const Shop = ({
   const Card = ({ prod }) => {
     return (
       <li>
-        <div>
-          <img src={prod.image[1]} alt="" />
+        <div className=" flex flex-col hover:cursor-pointer">
+          <div className=" w-80 h-80 flex justify-center items-center bg-slate-100">
+            <img src={prod.image[1]} alt="" />
+          </div>
+          <div className=" w-80 h-24 flex justify-between items-center">
+            <div className=" flex flex-col gap-1">
+              <h2 className=" text-lg font-medium">{prod.name}</h2>
+              <h3>{`\$${prod.price}`}</h3>
+            </div>
+            <div className=" flex items-end gap-1">
+              <div className=" pr-2 flex justify-center items-center">
+                <button className=" text-3xl">-</button>
+                <input
+                  type="text"
+                  defaultValue={0}
+                  className=" w-8 text-center p-1"
+                />
+                <button className=" text-3xl">+</button>
+              </div>
+              <div className=" pb-1">
+                <ShoppingCartOutlined />
+              </div>
+            </div>
+          </div>
         </div>
       </li>
     );
   };
 
   return (
-    <div>
-      <ul>
+    <div className=" p-4 flex justify-center items-center">
+      <ul className=" flex flex-wrap justify-center items-center gap-4">
         {Products.map((product) => {
           if (product.universe !== universe) return;
           if (searchValue !== "") {
