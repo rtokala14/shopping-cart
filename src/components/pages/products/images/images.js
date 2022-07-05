39f9c69c63664fa4813ecbf7ddb48be23d38487b
Rohit Tokala <rtokala@purdue.edu>
2022-07-05 17:14:47 +0400

    Add products and images for marvel

---
+++ src/components/pages/products/images/images.js
@@ -0,0 +1,62 @@
+const images = {
+  marvel: {
+    armorized_deadpool: [
+      require("./marvel.armorized-deadpool.jpg"),
+      require("./marvel.armorized-deadpool_th.jpg"),
+    ],
+    captain_america: [
+      require("./marvel.captain-america.jpg"),
+      require("./marvel.captain-america_th.jpg"),
+    ],
+    deadpool: [
+      require("./marvel.deadpool.jpg"),
+      require("./marvel.deadpool_th.jpg"),
+    ],
+    gambit_deluxe: [
+      require("./marvel.gambit-deluxe.jpg"),
+      require("./marvel.gambit-deluxe_th.jpg"),
+    ],
+    hulk_vs_hulkbuster: [
+      require("./marvel.hulk-vs-hulkbuster.jpg"),
+      require("./marvel.hulk-vs-hulkbuster_th.jpg"),
+    ],
+    iron_main_mark_xlii: [
+      require("./marvel.iron-man-mark-xliii.jpg"),
+      require("./marvel.iron-man-mark-xliii_th.jpg"),
+    ],
+    iron_man_neon_tech_40: [
+      require("./marvel.iron-man-neon-tech-40.jpg"),
+      require("./marvel.iron-man-neon-tech-40_th.jpg"),
+    ],
+    quicksilver: [
+      require("./marvel.quicksilver.jpg"),
+      require("./marvel.quicksilver_th.jpg"),
+    ],
+    spider_man: [
+      require("./marvel.spider-man.jpg"),
+      require("./marvel.spider-man_th.jpg"),
+    ],
+    spider_man_deluxe_version_special_edition: [
+      require("./marvel.spider-man-deluxe-version-special-edition.jpg"),
+      require("./marvel.spider-man-deluxe-version-special-edition_th.jpg"),
+    ],
+    spider_man_negative_zone_suit: [
+      require("./marvel.spider-man-negative-zone-suit.jpg"),
+      require("./marvel.spider-man-negative-zone-suit_th.jpg"),
+    ],
+    thor_sixth_scale_figure: [
+      require("./marvel.thor-sixth-scale-figure.jpg"),
+      require("./marvel.thor-sixth-scale-figure_th.jpg"),
+    ],
+    venomized_groot: [
+      require("./marvel.venomized-groot.jpg"),
+      require("./marvel.venomized-groot_th.jpg"),
+    ],
+    wolverine: [
+      require("./marvel.wolverine.jpg"),
+      require("./marvel.wolverine_th.jpg"),
+    ],
+  },
+};
+
+export default images;
